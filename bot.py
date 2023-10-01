907812ea19e1c8ac9d3769aff40eddc935c0a22e
QuantGeekDev <alex007d@gmail.com>
2023-10-01 13:58:51 +0200

    Add welcome message

--- bot.py
+++ bot.py
@@ -2,18 +2,23 @@ import config
 import logging
 import asyncio
 
-from aiogram import Bot, Dispatcher, types, Router
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
 
 telegramToken = config.TELEGRAM_TOKEN
 
-bookPrice = types.LabeledPrice(label="Comprar Ahora", amount=15*100)
 
 bot = Bot(token=telegramToken)
 dispatcher = Dispatcher(bot=bot)
 
 
+@dispatcher.message(F.text == "/start")
+async def command_start(message: Message):
+    await message.answer(text="Welcome to the test bot!")
+
+
 async def main():
     await dispatcher.start_polling(bot)
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
