c0e711991909db6ccf69b562e0cae4997b9db949
QuantGeekDev <alex007d@gmail.com>
2023-10-01 13:51:05 +0200

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import config
+import logging
+import asyncio
+
+from aiogram import Bot, Dispatcher, types, Router
+
+telegramToken = config.TELEGRAM_TOKEN
+
+bookPrice = types.LabeledPrice(label="Comprar Ahora", amount=15*100)
+
+bot = Bot(token=telegramToken)
+
+
+dispatcher = Dispatcher(bot=bot)
+
+
