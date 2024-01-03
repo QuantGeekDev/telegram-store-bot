ba8d7e4e1a091da0841f3c5413fedac6da975a34
Alex Andru <alex007d@gmail.com>
2024-01-03 21:52:38 +0100

    Add docstring to Abstract Book class

--- src/TelegramBookStore/Books/AbstractBook.py
+++ src/TelegramBookStore/Books/AbstractBook.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 @dataclass
 class AbstractBook(ABC):
+    '''Abstract Base Class for Book'''
     id: Optional[int]
     title: Optional[str]
     description: Optional[str]
